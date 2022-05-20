972a337a9762581e9a3f6056566675bc8766c91f
renpond <101608612+renpond@users.noreply.github.com>
2022-05-19 21:20:49 -0500

    Update dataTypes3.js

--- dataTypes3.js
+++ dataTypes3.js
@@ -1,4 +1,4 @@
-function formatDate(currentDate) {
+/*function formatDate(currentDate) {
   let hours = currentDate.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -67,7 +67,7 @@ function getForecast(coordinates) {
   let apiKey = "54cb345a2c3729ba77c24984961b3eee";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
-}
+}*/
 
 function displayWeather(response) {
   let cityElement = document.querySelector("#city");
