2af32ec1f1a33c4714224b53033d9ab4c89cc951
renpond <101608612+renpond@users.noreply.github.com>
2022-05-19 21:17:23 -0500

    Update dataTypes3.js

--- dataTypes3.js
+++ dataTypes3.js
@@ -25,7 +25,7 @@ let now = new Date();
 let currentDate = document.querySelector("p");
 currentDate.innerHTML = formatDate(now);
 
-/*function displayForecast(response) {
+function displayForecast(response) {
   let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
@@ -62,7 +62,7 @@ currentDate.innerHTML = formatDate(now);
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-*/
+
 function getForecast(coordinates) {
   let apiKey = "54cb345a2c3729ba77c24984961b3eee";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
